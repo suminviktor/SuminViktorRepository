2df386da99bac96bc695befb9bdf8c1fea12c1ef
suminviktor <suminviktor@mail.ru>
2014-09-07 15:57:56 +0600

    HelloWorld
    No description

---
+++ App1/App1/App1.cpp
@@ -0,0 +1,14 @@
+// App1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"Hello world";
+	system("pause");
+	return 0;
+}
+
